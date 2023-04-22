d0b71d3a4113fb8ba59465aeecd46b4cc2b6fa72
Heurtault Aubin <aub.heurtault@gmail.com>
2023-04-22 14:54:35 +0200

    init

---
+++ app/app.config.ts
@@ -0,0 +1,33 @@
+export default defineAppConfig({
+  awesome: {
+    name: 'Nuxt 3 Awesome Starter',
+    project: {
+      links: {
+        github: 'https://github.com/viandwi24/nuxt3-awesome-starter',
+      },
+    },
+    layout: {
+      page: {
+        navbar: {
+          menus: [
+            { type: 'link', title: 'Blank', to: { name: 'blank' } },
+            { type: 'link', title: 'Post', to: { name: 'post' } },
+            { type: 'link', title: 'Test', to: { name: 'test' } },
+            { type: 'button', title: 'Setting', to: { name: 'setting' } },
+          ],
+        },
+      },
+      footer: {
+        year: 2022,
+      },
+    },
+    author: {
+      name: 'viandwi24',
+      links: {
+        github: 'https://github.com/viandwi24',
+        medium: 'https://viandwi24.medium.com',
+        website: 'https://viandwi24.site',
+      },
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+definePageMeta({ layout: 'page' })
+useHead({ titleTemplate: '' })
+</script>
+
+<template>
+  <AwesomeWelcome :with-alert="false" />
+</template>
