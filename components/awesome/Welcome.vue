34a872975aa00fefe38fb48fc158640c99487ac5
Heurtault Aubin <aub.heurtault@gmail.com>
2023-04-22 15:15:44 +0200

    update doc

--- components/awesome/Welcome.vue
+++ components/awesome/Welcome.vue
@@ -1,193 +1,114 @@
 <script lang="ts" setup>
 const { awesome } = useAppConfig()
 
-defineProps({
-  withAlert: {
-    type: Boolean,
-    default: true,
-  },
-})
-
-const titlesText = computed<string[]>(() =>
-  'Nuxt 3[]Awesome[]Starter'.split('[]')
-)
-const leadingsText = computed(() => [
-  {
-    text: titlesText.value[0],
-    startColor: '#007CF0',
-    endColor: '#00DFD8',
-    delay: 0,
-  },
-  {
-    text: titlesText.value[1],
-    startColor: '#7928CA',
-    endColor: '#FF0080',
-    delay: 2,
-  },
-  {
-    text: titlesText.value[2],
-    startColor: '#FF4D4D',
-    endColor: '#F9CB28',
-    delay: 4,
-  },
-])
 </script>
 
 <template>
-  <LayoutPageWrapper class="flex-1 flex">
-    <LayoutPageSection class="flex-1 flex">
-      <div class="flex-1 flex flex-col items-center justify-center">
-        <h1 class="text-center mt-4">
-          <span
-            v-for="(item, i) in leadingsText"
-            :key="i"
-            :style="`--content: '${item.text}'; --start-color: ${
-              item.startColor
-            }; --end-color: ${item.endColor}; --animation-name: anim-fg-${
-              i + 1
-            }`"
-            class="animated-text-bg drop-shadow-xl text-6xl sm:text-8xl md:text-8xl lg:text-8xl 2xl:text-8xl block font-black uppercase"
+  <LayoutPageWrapper>
+    <LayoutPageSection>
+      <div
+        class="h-screen w-full py-10 sm:py-20 flex items-center sm:items-end"
+      >
+        <div class="mb-2">
+          <div class="grid grid-cols-12 gap-4">
+            <h1
+              class="col-span-12 sm:col-span-6 lg:col-span-8 xl:col-span-9 text-4xl md:text-5xl xl:text-8xl font-title font-medium tracking-normal pb-20"
+            >
+              Studio expérimental <br />
+              & design graphique
+            </h1>
+            <div class="col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3">
+              <div class="mt-12">
+                <hr class="mb-4 text-blackÒ" />
+                <p class="font-ibm text-left">
+                  DBF, c’est un studio qui est parti d’un projet sur les rêves (Voir Mirage). Et puis on s’est dit : «pourquoi pas faire un studio ?» alors on l’a fait. Venez découvrir DBF. La flèche <br/> juste ici là. Là.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="flex flew-row w-full justify-between flex-end pb-20 pt-20"
           >
-            <span class="animated-text-fg">{{ item.text }}</span>
-          </span>
-        </h1>
-        <div class="px-4 mt-6 text-center max-w-[500px] md:max-w-[600px]">
-          a starter template for Nuxt 3 with minimalist themes design, built in
-          components, drawer & menus, and more.
+            <img class="" src="/images/domicile/1.png" />
+            <img class="h-30 w-60" src="/images/domicile/2.png" />
+            <img class="h-30 w-60" src="/images/domicile/3.png" />
+            <img class="h-30 w-60" src="/images/domicile/4.png" />
+            <img class="h-30 w-60" src="/images/domicile/5.png" />
+            <img class="h-30 w-60" src="/images/domicile/6.png" />
+          </div>
         </div>
         <div
-          v-if="withAlert"
-          class="mt-4 w-auto text-center text-white bg-slate-800 rounded px-4 py-1 text-sm"
+          class="absolute bottom-0 left-0 right-0 w-full flex flex-row justify-center p-4 bg-customGray"
         >
-          create file "~/pages/index.vue" to replace this page
-        </div>
-        <div class="flex space-x-4 ml-2 mt-8 justify-center md:justify-start">
-          <AwesomeButton
-            size="lg"
-            text="Nuxt 3"
-            class="font-extrabold"
-            href="https://nuxt.com"
-            _target="blank"
-          />
-          <AwesomeButton
-            v-if="awesome?.project?.links?.github"
-            size="lg"
-            text="Github"
-            type="secondary"
-            class="font-extrabold"
-            :href="awesome?.project?.links?.github"
-            _target="blank"
-          />
+          Studio fondé en 2022 à Rennes par Gauthier Dumont, Guillaume Bour &
+          Mathilde Faure, dans le cadre du Master 2 Directeur Artistique et
+          Numérique LISAA de Rennes.
         </div>
       </div>
-      <div class="top-0 left-0 absolute w-screen">
-        <div class="absolute right-0 top-0 w-1/4 h-screen py-10 pt-12 z-0 flex">
-          <div
-            class="flex-1 rounded-l-9xl bg-gradient-to-l from-blue-600/10"
-          ></div>
+      <p class="text-3xl font-medium pb-2">Petite Histoire</p>
+      <hr class="mb-4" />
+      <p class="font-ibm italic xl:w-1/2 text-base mb-10">
+        Gauthier a fait une licence gagna à Angers. Guillaume a multiplié les
+        expériences et formations à Nior, Bordeaux, Nantes et Metz. Mathilde,
+        fantôme à LISAA Rennes depuis 5 ans. Ils n’étaient pas destinés à se
+        rencontrer, et pourtant les voici tous trois réunis à travailler pour
+        obtenir leur diplôme final : le Mastère. Travailler ensemble coule de
+        source, c’est pourquoi le studio graphique est né. Né d’une idée
+        saugrenue de prolonger l’expérience du rêve, avant de rencontrer Tristan
+        Moir et de se rendre compte que l’idée n’est pas saugrenue du tout. Elle
+        est même carrément révolutionnaire (mais là, on s’emballe peut-être un
+        peu). Pourquoi donc s’arrêter à un seul projet, lorsque l’on peut créer
+        Studio DBF ?"
+      </p>
+      <p class="text-3xl font-medium pb-2">Les valeurs du studio</p>
+      <hr class="mb-4" />
+      <div class="flex flex-row wrap mb-5">
+        <div>
+          <p class="font-bold">Déconstruire</p>
+          <p class="font-ibm text-left">
+            Chaque sujet est décortiqué et analysé, afin de prendre du recul sur
+            celui-ci et de choisir un axe innovant. Objectif de se démarquer en
+            proposant une nouvelle approche, auquel on ne pense pas forcément.
+          </p>
         </div>
-        <div class="absolute left-0 top-0 w-1/4 h-screen py-10 pt-12 z-0 flex">
-          <div
-            class="flex-1 rounded-r-9xl bg-gradient-to-r from-green-600/10"
-          ></div>
+        <div>
+          <p class="font-bold">Bousculer</p>
+          <p class="font-ibm text-left">
+            On aime bousculer, cela ne fait aucun doute. Notre mantra est de
+            toujours innover, proposer de nouvelles choses, et faire comprendre
+            au client que le graphisme n’a aucune limite.
+          </p>
+        </div>
+        <div>
+          <p class="font-bold">Façonner</p>
+          <p class="font-ibm text-left">
+            Terme artisanal. Le studio est composé de passionnées qui aiment
+            toucher à tout. Mais surtout, qui ne délaissera jamais le print au
+            profit du web. Ça, JAMAIS : le graphiste est avant tout un artisan.
+          </p>
+        </div>
+      </div>
+      <p class="text-3xl pb-2">Nos projets</p>
+      <hr class="mb-4" />
+      <div class="flex flex-col w-full">
+        <div class="flex flex-row w-full py-2 gap-3">
+          <AwesomeImages class="w-1/3" img="/images/domicile/7.png" />
+          <AwesomeImages class="w-2/3" img="/images/domicile/8.png" />
+        </div>
+        <div class="flex flex-row w-full py-2 gap-3">
+          <AwesomeImages class="w-1/2" img="/images/domicile/9.png" />
+          <AwesomeImages class="w-1/2" img="/images/domicile/10.png" />
+        </div>
+        <div class="flex flex-row w-full py-2 gap-3">
+          <AwesomeImages class="w-1/3" img="/images/domicile/11.png" />
+          <AwesomeImages class="w-1/3" img="/images/domicile/12.png" />
+          <AwesomeImages class="w-1/3" img="/images/domicile/13.png" />
+        </div>
+        <div class="flex flex-row w-full py-2 gap-3">
+          <AwesomeImages class="w-2/3" img="/images/domicile/14.png" />
+          <AwesomeImages class="w-1/3" img="/images/domicile/15.png" />
         </div>
       </div>
     </LayoutPageSection>
   </LayoutPageWrapper>
 </template>
-
-<style lang="scss">
-:root {
-  --padding: 0.05em;
-}
-
-@keyframes anim-fg-1 {
-  0%,
-  16.667%,
-  100% {
-    opacity: 1;
-  }
-  33.333%,
-  83.333% {
-    opacity: 0;
-  }
-}
-@keyframes anim-fg-2 {
-  0%,
-  16.667%,
-  66.667%,
-  100% {
-    opacity: 0;
-  }
-  33.333%,
-  50% {
-    opacity: 1;
-  }
-}
-@keyframes anim-fg-3 {
-  0%,
-  50%,
-  100% {
-    opacity: 0;
-  }
-  66.667%,
-  83.333% {
-    opacity: 1;
-  }
-}
-.animated-text-bg {
-  position: relative;
-  display: block;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  content: var(--content);
-  display: block;
-  width: 100%;
-  color: theme('colors.slate.800');
-  top: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 0;
-  padding-left: var(--padding);
-  padding-right: var(--padding);
-  &:before {
-    content: var(--content);
-    position: absolute;
-    display: block;
-    width: 100%;
-    color: theme('colors.slate.800');
-    top: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 0;
-    padding-left: var(--padding);
-    padding-right: var(--padding);
-  }
-}
-.animated-text-fg {
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  padding-left: var(--padding);
-  padding-right: var(--padding);
-  background-image: linear-gradient(
-    90deg,
-    var(--start-color),
-    var(--end-color)
-  );
-  position: relative;
-  opacity: 0;
-  z-index: 1;
-  animation: var(--animation-name) 8s infinite;
-}
-html.dark {
-  .animated-text-bg {
-    color: theme('colors.gray.100');
-    &:before {
-      color: theme('colors.gray.100');
-    }
-  }
-}
-</style>
