a2a6dc55abf76d5a33d6dbfd52626aae28783366
Heurtault Aubin <aub.heurtault@gmail.com>
2023-04-23 16:06:14 +0200

    add contact

---
+++ components/awesome/Contact.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div class="div1 col-span-1 row-span-2">
+    <div class="bg-customGray rounded-md w-full h-96 p-6">
+      <div class="grid grid-cols-4 gap-2">
+        <div class="relative z-0 col-span-2">
+          <input
+            id="floating_standard"
+            type="text"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+          />
+          <label
+            for="floating_standard"
+            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 h-full"
+          >Nom</label
+          >
+        </div>
+        <div class="relative z-0 col-span-2">
+          <input
+            id="floating_standard"
+            type="text"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+          />
+          <label
+            for="floating_standard"
+            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 h-full"
+          >Mail</label
+          >
+        </div>
+        <div class="relative z-0 col-span-2">
+          <input
+            id="floating_standard"
+            type="text"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+          />
+          <label
+            for="floating_standard"
+            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >Sujet</label
+          >
+        </div>
+      </div>
+      <div class="relative z-0 h-full">
+        <input
+          id="floating_standard"
+          type="text"
+          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+        />
+        <label
+          for="floating_standard"
+          class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >Message</label
+        >
+      </div>
+    </div>
+  </div>
+</template>
--- pages/frequentation.vue
+++ pages/frequentation.vue
@@ -1,3 +1,39 @@
+<script lang="ts" setup>
+definePageMeta({ layout: 'page' })
+useHead({ titleTemplate: 'Mirage' })
+</script>
 <template>
+  <LayoutPageWrapper>
+    <LayoutPageSection>
+      <div class="w-full flex">
+        <div class="mb-2">
+          <div class="grid grid-cols-2 grid-rows-2 gap-0">
+            <div class="div2 col-span-1 row-span-1">
+              <div class="flex flex-col">
+                <h1
+                  class="text-4xl md:text-5xl xl:text-8xl font-title font-medium tracking-normal sm:pb-20"
+                >
+                  Un Projet ?<br />
+                  Une Idée ?
+                </h1>
+                <div class="px-24 ">
+                  <hr class="mb-4 dark:border-white" />
+                  <p class="font-ibm text-left text-sm sm:text-base ">
+                    DBF, c’est un studio qui est parti d’un projet sur les rêves
+                    (Voir Mirage). Et puis on s’est dit : «pourquoi pas faire un
+                    studio ?» alors on l’a fait. Venez découvrir DBF. La flèche
+                    <br />
+                    juste ici là. Là.
+                  </p>
+                </div>
+              </div>
 
+            </div>
+            <AwesomeContact/>
+
+          </div>
+        </div>
+      </div>
+    </LayoutPageSection>
+  </LayoutPageWrapper>
 </template>
