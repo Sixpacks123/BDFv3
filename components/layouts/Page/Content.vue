403ad18af789a141ba82adda0c257fed5c030bda
Heurtault Aubin <aub.heurtault@gmail.com>
2023-05-29 19:21:31 +0200

    margin update

--- components/layouts/Page/Content.vue
+++ components/layouts/Page/Content.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex w-full pt-[0px]"
+    class="flex w-full pt-[0px] px-10"
     :style="{ 'min-height': 'var(--layout-page-content-min-height)' }"
   >
     <slot />
