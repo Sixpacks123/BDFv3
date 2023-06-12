563b5e8029d3e80934d71dbadf5c0731944201da
Heurtault Aubin <38321768+Sixpacks123@users.noreply.github.com>
2023-06-12 11:54:22 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     'nuxt-headlessui',
     'nuxt-icon',
-    '@nuxtjs/color-mode',
+   
     // management
     '@pinia/nuxt',
     // contents,
